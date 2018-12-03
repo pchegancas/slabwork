e7d76a5e6dd0ba4c054bc7895a32778b647578a1
pchegancas <45571156+pchegancas@users.noreply.github.com>
2018-12-03 16:27:51 -0500

    Update MySlabs.py

--- MySlabs.py
+++ MySlabs.py
@@ -6,22 +6,39 @@ class Slab():
 	"""
 	Model of a slab
 	"""
-	def __init__ (self, name: str, thk: float, concrete: float)
+	def __init__ (self, name: str, thk: float, concrete: float):
 		self.name = name
 		self.thk = thk
 		self.concrete = concrete
 	
-	def __repr__ (self)
+	def __repr__ (self):
 		print(f'Slab({self.name}, {self.thk}, {self.concrete})')
 
 class Band():
 	"""
 	Model of a band
 	"""
-	def __init__(self, slab: Slab, width: float, support)
+	def __init__(self, slab: Slab, width: float, strip_type):
 		self.slab = slab
 		self.width = width
-		self.support = support
+		self.strip_type = strip_type
 	
-	def __repr__ (self)
-		print(f'Band({self.slab}, {self.width}, {self.support})')
+	def __repr__ (self):
+		print(f'Band({self.slab}, {self.width}, {self.strip_type})')
+	
+	def get_min_reinforcement(self):
+		return self.width * self.slab.thk * 0.002
+		
+class Project():
+	"""
+	
+	"""
+	def __init__(self, name: str, number: str, date):
+		self.name = name
+		self.number = number
+		self.date = date
+	
+	def __repr__ (self):
+		print(f'Project({self.name}, {self.number}, {self.date})')
+		
+	
